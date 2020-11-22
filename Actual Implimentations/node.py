bbc96053baa2b77d00a316348a2a6fbf90ebeda0
dalekvim <Dalekvim@gmail.com>
2020-11-22 13:55:50 +0000

    removed unnecessary components

--- Actual Implimentations/node.py
+++
@@ -1,4 +0,0 @@
-class Node:
-    def __init__(self, value=None, _next=None):
-        self.value = value
-        self.next = _next
--- Noob Attempt/README.md
+++
@@ -1,2 +0,0 @@
-# data_structures
-The data structures I made while learning about them with tests inculuded! Finally right?
